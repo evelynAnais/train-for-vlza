70633a2bf95ca8dc3d67c9b49b87d4e810dcc742
evelynAnais <evelyn.a.n.d.30@gmail.com>
2023-07-05 21:18:51 -0600

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { StepList, CreateStep } from './sections';
 import { Footer, Header } from './components';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
